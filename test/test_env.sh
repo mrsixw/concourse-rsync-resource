8f7e5c9d49c9233514ef3830515e5390027f4ce9
Steve Williams <stevenw2@cisco.com>
2016-06-20 16:55:05 +0100

    Add implementation to out and test environment.

---
+++ test/test_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export SRC_DIR=$PWD
+export BUILD_ID=1
+export BUILD_NAME='my-build'
+export BUILD_JOB_NAME='my-build-job'
+export BUILD_PIPELINE_NAME='my-build-pipeline'
+export ATC_EXTERNAL_URL='127.0.0.1/atc'
